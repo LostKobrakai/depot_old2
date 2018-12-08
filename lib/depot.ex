76fe4b98dc7c127bcb9d5368c04402e2f1528fb8
Benjamin Milde <benni@kobrakai.de>
2018-12-08 16:40:35 +0100

    Initial Commit

---
+++ lib/depot.ex
@@ -0,0 +1,18 @@
+defmodule Depot do
+  @moduledoc """
+  Documentation for Depot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Depot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
